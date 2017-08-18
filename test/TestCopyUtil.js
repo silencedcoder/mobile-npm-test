3b9b670c921f6ba7c8fbe10be171bee3007e8778
sunsong <song.sun@newtouch.cn>
2017-08-18 10:26:36 +0800

    提交测试文件

---
+++ test/TestCopyUtil.js
@@ -0,0 +1,22 @@
+/**
+ * Created by mac on 2017/8/18.
+ */
+// import {cascadingCopy} from '../src/CopyUtil'
+
+// var fn = require('../src/CopyUtil');
+//
+//
+// let obj = new Array();
+// let a = ['1',2,'3']
+// let b = {a:'1',b:'2'}
+// let c = {a:['1',2,'3'],b:{a:'1',b:'2'}}
+//
+// obj.push(a);
+// obj.push(b);
+// obj.push(c);
+//
+// console.log(obj);
+//
+// let copyObj = fn.cascadingCopy(obj);
+//
+// console.log(copyObj);
